3eff92788d23634a60d9e338caaae56b198dea10
Rudra pratap Singh <rudrapratapsingh@192.168.0.105>
2025-08-18 06:26:52 +0530

    Fix all image imports and deploy setup

--- src/component/hero.jsx
+++ src/component/hero.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import profileImage from '../assets/rudra-B_87ej3m.jpeg'; // exact file name
 
-function Hero() {
+import React from 'react';
+import profileImage from '../assets/rudra-B_87ej3m.jpeg'; // exact filename
+
+const Hero = () => {
   return (
     <div>
       <img src={profileImage} alt="Rudra Pratap Singh" />
       <h1>Welcome to my Portfolio</h1>
     </div>
   );
-}
+};
 
 export default Hero;
