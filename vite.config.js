7dd3fa623125ed189e70bb6c9809e0cb99e196ce
Rudra pratap Singh <rudrapratapsingh@192.168.0.105>
2025-08-18 06:18:08 +0530

    Fix imports and deploy setup

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/portfolio-1/', // must exactly match your GitHub repo name
+  base: '/portfolio-1/', // EXACT GitHub repo name
   plugins: [react()],
 });
